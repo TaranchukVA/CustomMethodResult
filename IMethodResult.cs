b69ad7e58be7974fc0bbc6e636c524c04bb9f3b4
Vladimir Taranchuk <VladimirTaranchuk@gmail.com>
2022-12-04 14:38:17 +0300

    Добавьте файлы проекта.

---
+++ IMethodResult.cs
@@ -0,0 +1,26 @@
+﻿namespace CustomMethodResult
+{
+    /// <summary>
+    /// Author: Vladimir Taranchuk.<br/>
+    /// Author's Git: TaranchukVA.<br/>
+    /// Author's LinkedIn: taranchuk. 
+    /// </summary>
+    /// <typeparam name="T">Type of result</typeparam>
+    public interface IMethodResult<T>
+    {
+        /// <summary>
+        /// Gets a value that indicates if the method was successful
+        /// </summary>
+        public bool IsSuccess { get; }
+
+        /// <summary>
+        /// Gets the message with main text information of method result
+        /// </summary>
+        public string Message { get; }
+
+        /// <summary>
+        /// Gets the data that method returned
+        /// </summary>
+        public T Data { get; }
+    }
+}
---
+++ MethodResult.cs
@@ -0,0 +1,31 @@
+﻿namespace CustomMethodResult
+{
+    /// <summary>
+    /// Author: Vladimir Taranchuk.<br/>
+    /// Author's Git: TaranchukVA.<br/>
+    /// Author's LinkedIn: taranchuk.
+    /// 
+    /// </summary>
+    /// <typeparam name="T">Type of result</typeparam>
+    public class MethodResult<T> : IMethodResult<T>
+    {
+        public bool IsSuccess { get; }
+        public string Message { get; }
+        public T Data { get; } = default;
+
+        /// <param name="isSuccess">If does not declare success, it sets false</param>
+        public MethodResult(bool isSuccess = false) =>
+            IsSuccess = isSuccess;
+
+        /// <param name="isSuccess">If does not declare success, it sets false</param>
+        /// <param name="message">If message is null, it sets empty string</param>
+        public MethodResult(string message, bool isSuccess = false) : this(isSuccess) =>
+            Message = message ?? string.Empty;
+
+        /// <param name="isSuccess">If does not declare success, it sets false</param>
+        /// <param name="message"> If message is null, it sets empty string </param>
+        public MethodResult(T data, string message = null, bool isSuccess = false) : this(message, isSuccess) =>
+            Data = data;
+
+    }
+}
